7c9798182483b10707995a504eef897ffd0aacbf
Emílio Biasi <ejbiasi@hotmail.com>
2022-03-13 21:49:06 -0300

    adicionando tela consultar veiculo

---
+++ app/src/main/java/br/com/appzonaazul/ConsultaVeiculoActivity.kt
@@ -0,0 +1,11 @@
+package br.com.appzonaazul
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ConsultaVeiculoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_consulta_veiculo)
+    }
+}
