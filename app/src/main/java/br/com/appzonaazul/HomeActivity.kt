650237db2d96b5becf20aafc6068b242dc207e46
Emílio Biasi <ejbiasi@hotmail.com>
2022-03-14 21:03:50 -0300

    arrumando erro de binding

--- app/src/main/java/br/com/appzonaazul/HomeActivity.kt
+++ app/src/main/java/br/com/appzonaazul/HomeActivity.kt
@@ -2,20 +2,13 @@ package br.com.appzonaazul
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import br.com.appzonaazul.databinding.ActivityHomeBinding
+
 
 class HomeActivity : AppCompatActivity() {
-
-    private lateinit var binding: ActivityHomeBinding
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        //inflar a activity
-        binding = ActivityHomeBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-
+        setContentView(R.layout.activity_home)
     }
 }
