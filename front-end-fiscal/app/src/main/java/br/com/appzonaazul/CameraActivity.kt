9da283eec4a804d35b55b57421fda7d14b3dc773
Elias-FS <elias-fausto@hotmail.com>
2022-04-29 20:12:05 -0300

    Atualização de camera

--- front-end-fiscal/app/src/main/java/br/com/appzonaazul/CameraActivity.kt
+++ front-end-fiscal/app/src/main/java/br/com/appzonaazul/CameraActivity.kt
@@ -1,72 +1,77 @@
 package br.com.appzonaazul
 
+import android.app.Activity
 import android.content.Intent
+import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.activity.result.contract.ActivityResultContracts
-import br.com.appzonaazul.databinding.ActivityCameraBinding
-import com.google.android.material.snackbar.Snackbar
+import android.os.Environment
+import android.provider.MediaStore
+import android.widget.Toast
+import androidx.appcompat.widget.AppCompatButton
+import androidx.appcompat.widget.AppCompatImageView
+import androidx.appcompat.widget.AppCompatTextView
+import androidx.core.content.FileProvider
+import java.io.File
+import java.time.LocalDateTime
 
+private const val FILE_NAME = "photo.jpg"
+private const val REQUEST_CODE = 42
+private lateinit var photoFile: File
 class CameraActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityCameraBinding
+
+    private lateinit var tvData: AppCompatTextView
+    private lateinit var btnTirarFoto : AppCompatButton
+    private lateinit var ivFoto : AppCompatImageView
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_camera)
 
-        //inflar a activity
-        binding = ActivityCameraBinding.inflate(layoutInflater)
-        setContentView(binding.root)
 
-        // Navegação dos botões da barra de menu
-        binding.btnNavigation.setOnNavigationItemSelectedListener { item ->
-            when(item.itemId) {
-                R.id.btnNavigationItinerario -> {
-                    // Respond to navigation item 1 click
-                    openWindowItinerario()
-                    true
-                }
-                R.id.btnNavigationConsultar -> {
-                    // Respond to navigation item 2 click
-                    abrirTelaConsultarVeiculo()
-                    true
-                }
-                else -> false
+        tvData  = findViewById(R.id.tvData)
+        ivFoto = findViewById(R.id.ivFoto)
+        btnTirarFoto = findViewById(R.id.btnTirarFoto)
+
+
+        btnTirarFoto.setOnClickListener {
+            val tirarFoto = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            photoFile = getPhotoFile(FILE_NAME)
+            pegarData()
+
+            //tirarFoto.putExtra(MediaStore.EXTRA_OUTPUT, photoFile)
+            val fileProvider = FileProvider.getUriForFile(this, "br.com.appzonaazul.fileprovider", photoFile)
+            tirarFoto.putExtra(MediaStore.EXTRA_OUTPUT, fileProvider)
+            if (tirarFoto.resolveActivity(this.packageManager) != null) {
+                startActivityForResult(tirarFoto, REQUEST_CODE)
+            } else {
+                Toast.makeText(this, "Unable to open camera", Toast.LENGTH_SHORT).show()
             }
         }
+    }
 
+    private fun getPhotoFile(fileName: String): File {
+        // Use "getExternalFilesDir" on Context to access package-especific directories
 
-        binding.btnOpenCamera.setOnClickListener {
-            //solicitar permissão da CAMERA
-            cameraProviderResult.launch(android.Manifest.permission.CAMERA)
+        val storageDirectory = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        return File.createTempFile(fileName, ".jpg", storageDirectory)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == REQUEST_CODE && resultCode == Activity.RESULT_OK){
+            //val pegarfoto = data?.extras?.get("data") as Bitmap
+            val pegarfoto = BitmapFactory.decodeFile(photoFile.absolutePath)
+            ivFoto.setImageBitmap(pegarfoto)
+        }else {
+            super.onActivityResult(requestCode, resultCode, data)
         }
-    }
-
-
-        private val cameraProviderResult =
-            registerForActivityResult(ActivityResultContracts.RequestPermission()){
-                if(it){
-                    abrirTelaPreview()
-                }else{
-                    Snackbar.make(binding.root, "Você não concedeu permissões para usar a câmera.", Snackbar.LENGTH_INDEFINITE).show()
-                }
-            }
-
-        private fun abrirTelaPreview() {
-            //navegar para a outra activity
-            val intentCameraPreview = Intent(this, CameraPreviewActivity::class.java)
-            startActivity(intentCameraPreview)
-    }
-
-    private fun abrirTelaConsultarVeiculo() {
-        //navegar para a outra activity
-        val intentConsultaVeiculo = Intent(this, ConsultaVeiculoActivity::class.java)
-        startActivity(intentConsultaVeiculo)
 
     }
-    private fun openWindowItinerario() {
-        //navegar para a outra activity
-        val openItinerario = Intent(this, MapsActivity::class.java)
-        startActivity(openItinerario)
+
+    private fun pegarData(){
+        val data = LocalDateTime.now()
+        tvData.text = data.toString()
     }
 
 
