d53e3d6b418f80320441d3ef0f94f74d71b723c3
Elias-FS <elias-fausto@hotmail.com>
2022-05-01 01:58:17 -0300

    Atualização de camera 2.0

--- front-end-fiscal/app/src/main/java/br/com/appzonaazul/CameraActivity.kt
+++ front-end-fiscal/app/src/main/java/br/com/appzonaazul/CameraActivity.kt
@@ -3,26 +3,35 @@ package br.com.appzonaazul
 import android.app.Activity
 import android.content.Intent
 import android.graphics.BitmapFactory
+import android.graphics.drawable.BitmapDrawable
+import android.graphics.drawable.Drawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Environment
 import android.provider.MediaStore
+import android.util.Log
 import android.widget.Toast
-import androidx.appcompat.widget.AppCompatButton
-import androidx.appcompat.widget.AppCompatImageView
-import androidx.appcompat.widget.AppCompatTextView
+import androidx.appcompat.widget.*
 import androidx.core.content.FileProvider
 import java.io.File
 import java.time.LocalDateTime
 
-private const val FILE_NAME = "photo.jpg"
+
 private const val REQUEST_CODE = 42
-private lateinit var photoFile: File
+
 class CameraActivity : AppCompatActivity() {
 
+    private var FILE_NAME = "photo.jpg"
+    private var paths: MutableList<String> = mutableListOf("","","","")
     private lateinit var tvData: AppCompatTextView
-    private lateinit var btnTirarFoto : AppCompatButton
-    private lateinit var ivFoto : AppCompatImageView
+    private lateinit var btnEnviarFoto : AppCompatButton
+    private lateinit var ivFoto : AppCompatImageButton
+    private lateinit var ivFoto2 : AppCompatImageButton
+    private lateinit var ivFoto3 : AppCompatImageButton
+    private lateinit var ivFoto4 : AppCompatImageButton
+    private lateinit var photoFile: File
+
+
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,38 +41,88 @@ class CameraActivity : AppCompatActivity() {
 
         tvData  = findViewById(R.id.tvData)
         ivFoto = findViewById(R.id.ivFoto)
-        btnTirarFoto = findViewById(R.id.btnTirarFoto)
+        ivFoto2 = findViewById(R.id.ivFoto2)
+        ivFoto3 = findViewById(R.id.ivFoto3)
+        ivFoto4 = findViewById(R.id.ivFoto4)
+        btnEnviarFoto = findViewById(R.id.btnEnviarFoto)
 
+        ivFoto.setOnClickListener(){
+            pegarFoto(1)
+        }
+        ivFoto2.setOnClickListener(){
+            pegarFoto(2)
+        }
+        ivFoto3.setOnClickListener(){
+            pegarFoto(3)
+        }
+        ivFoto4.setOnClickListener(){
+            pegarFoto(4)
+        }
+        btnEnviarFoto.setOnClickListener(){
+            if(paths[0].isEmpty()||paths[1].isEmpty()||paths[2].isEmpty()||paths[3].isEmpty()) {
+                Toast.makeText(
+                    baseContext,
+                    "!!!ERRO!!!Por favor, tire as quatro fotos antes de enviar!",
+                    Toast.LENGTH_SHORT
+                ).show()
+                pegarData()
+            } else{
 
-        btnTirarFoto.setOnClickListener {
-            val tirarFoto = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-            photoFile = getPhotoFile(FILE_NAME)
-            pegarData()
-
-            //tirarFoto.putExtra(MediaStore.EXTRA_OUTPUT, photoFile)
-            val fileProvider = FileProvider.getUriForFile(this, "br.com.appzonaazul.fileprovider", photoFile)
-            tirarFoto.putExtra(MediaStore.EXTRA_OUTPUT, fileProvider)
-            if (tirarFoto.resolveActivity(this.packageManager) != null) {
-                startActivityForResult(tirarFoto, REQUEST_CODE)
-            } else {
-                Toast.makeText(this, "Unable to open camera", Toast.LENGTH_SHORT).show()
             }
+        }
+
+
+    }
+
+    private fun pegarFoto(REQUEST_CODE: Int){
+        val tirarFoto = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        photoFile = getPhotoFile(FILE_NAME)
+
+        //tirarFoto.putExtra(MediaStore.EXTRA_OUTPUT, photoFile)
+        val fileProvider = FileProvider.getUriForFile(this, "br.com.appzonaazul.fileprovider", photoFile)
+        tirarFoto.putExtra(MediaStore.EXTRA_OUTPUT, fileProvider)
+        if (tirarFoto.resolveActivity(this.packageManager) != null) {
+            startActivityForResult(tirarFoto, REQUEST_CODE)
+        } else {
+            Toast.makeText(this, "Erro ao abrir a camera", Toast.LENGTH_SHORT).show()
         }
     }
 
     private fun getPhotoFile(fileName: String): File {
         // Use "getExternalFilesDir" on Context to access package-especific directories
-
         val storageDirectory = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
         return File.createTempFile(fileName, ".jpg", storageDirectory)
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        if (requestCode == REQUEST_CODE && resultCode == Activity.RESULT_OK){
-            //val pegarfoto = data?.extras?.get("data") as Bitmap
-            val pegarfoto = BitmapFactory.decodeFile(photoFile.absolutePath)
-            ivFoto.setImageBitmap(pegarfoto)
-        }else {
+        if (resultCode == Activity.RESULT_OK){
+            val bitmap = BitmapFactory.decodeFile(photoFile.absolutePath)
+            val img: Drawable = BitmapDrawable(resources,bitmap)
+            if(requestCode == 1) {
+                ivFoto.background = img
+                ivFoto.setImageIcon(null)
+                paths[0] = photoFile.absolutePath
+                Log.i("ENDERECO",paths[0])
+            }
+            if(requestCode == 2) {
+                ivFoto2.background = img
+                ivFoto2.setImageIcon(null)
+                paths[1] = photoFile.absolutePath
+                Log.i("ENDERECO",paths[1])
+            }
+            if(requestCode == 3) {
+                ivFoto3.background = img
+                ivFoto3.setImageIcon(null)
+                paths[2] = photoFile.absolutePath
+                Log.i("ENDERECO",paths[2])
+            }
+            if(requestCode == 4) {
+                ivFoto4.background = img
+                ivFoto4.setImageIcon(null)
+                paths[3] = photoFile.absolutePath
+                Log.i("ENDERECO",paths[3])
+            }
+        } else {
             super.onActivityResult(requestCode, resultCode, data)
         }
 
